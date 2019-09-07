b8b26642f1eca828b83e97cb56b9596af56a911f
wuming <928118279@qq.com>
2019-09-07 20:58:45 +0800

    实现分页和分页条数的切换，完成第一页分页的数组

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -13,10 +13,10 @@
         <!-- 航班信息 -->
         <div>
           <FlightsItem
-          v-for="(item,index) in flightsData.flights"
+          v-for="(item,index) in dataList"
           :key="index"
           :data="item">
-
+          </FlightsItem>
           <!-- 分页 -->
           <!-- size-change：每页条数切换时候触发 -->
           <!-- current-change：页码切换时候触发 -->
@@ -31,9 +31,7 @@
             :page-size="pageSize"
             layout="total, sizes, prev, pager, next, jumper"
             :total="total">
-          </el-pagination>
-
-          </FlightsItem>
+          </el-pagination> 
         </div>
       </div>
 
@@ -55,6 +53,8 @@ export default {
     return {
           // 机票列表返回的总数据，总数据包含4个属性，flights/info/options/tatol
           flightsData: {},
+          // 当前显示的列表数组 
+          dataList: {},
           pageIndex:1, // 当前的页码
           pageSize: 5, // 当前的条数
           total: 0     // 总条数
@@ -75,15 +75,32 @@ export default {
     }).then(res => {
       // 赋值给总数据
       this.flightsData = res.data
+
+      // 分页的总条数
+      this.total = this.flightsData.flights.length
+
+      // 第一页的值
+      this.dataList = this.flightsData.flights.slice(0,this.pageSize)
     })
   },
 
   methods: {
-    // 每页条数切换时候触发
-    handleSizeChange(){
+    // 每页条数切换时候触发,val是条数
+    handleSizeChange(val){
+      this.pageSize = val
+
+      // 按照数学公式切换回dataList的值
+      this.dataList = this.flightsData.flights.slice(0,val)
     },
-    // 页码切换时候触发
-    handleCurrentChange(){
+    // 页码切换时候触发,val是点击的页码
+    handleCurrentChange(val){
+      this.pageIndex = val //当前页
+
+      // 按照数学公式切换dataList的值
+      this.dataList = this.flightsData.flights.slice(
+        (this.pageIndex - 1) * this.pageSize,
+        this.pageIndex * this.pageSize
+      )
     }
   }
 }
